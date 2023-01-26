a4e5a1a9a702a8d89a82eb6e66d5922199ba0785
roman <rivanov@bivgrop.ru>
2023-01-26 22:43:17 +0300

    Fix EmailVerifier entity

--- domains/emailverifier.go
+++ domains/emailverifier.go
@@ -4,9 +4,8 @@ import "time"
 
 type EmailVerifier struct {
 	Id           uint64    `db:"id"`
+	Code         string    `db:"code"`
 	Email        string    `db:"email"`
-	CreateDate   time.Time `db:"createdate"`
-	StateId      uint64    `db:"stateid"`
-	TypeId       uint64    `db:"typeid"`
 	VerifyCodeTo time.Time `db:"verify_code_to"`
+	StateId      uint64    `db:"stateid"`
 }
