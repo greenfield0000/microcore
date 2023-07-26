9c0bcadec56855b51b38c8085add0dc2c7bcea1b
roman <rivanov@bivgrop.ru>
2023-07-26 21:30:08 +0300

    Change Email verify

--- middleware/email/email-middleware.go
+++ middleware/email/email-middleware.go
@@ -9,12 +9,14 @@ import (
 
 var emailVerifierCheckerIsNull = fmt.Errorf("Необходимо передать кинфигурацию для проверки")
 
+// EmailVerifyCheckConfig базовая конфигурация проверки на верифицированную почту1
 type EmailVerifyCheckConfig struct {
 	VerificationService service.EmailVerifierService
 	CheckFunction       func(c *fiber.Ctx, srv service.EmailVerifierService) error
 }
 
-func New(config EmailVerifyCheckConfig) fiber.Handler {
+// MailMustBeConfirmed почта должна быть подтверждена
+func MailMustBeConfirmed(config EmailVerifyCheckConfig) fiber.Handler {
 	return func(c *fiber.Ctx) error {
 		verification := config.VerificationService
 		if verification == nil {
