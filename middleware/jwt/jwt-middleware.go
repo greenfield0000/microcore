0847b06b3096c8fcc8dfe9230efa1a5941efe057
roman <rivanov@bivgrop.ru>
2023-07-26 22:07:43 +0300

    Add log for AuthRequired

--- middleware/jwt/jwt-middleware.go
+++ middleware/jwt/jwt-middleware.go
@@ -1,6 +1,7 @@
 package middleware
 
 import (
+	"fmt"
 	"github.com/gofiber/fiber/v2"
 	jwtware "github.com/gofiber/jwt/v3"
 	httpcommon "github.com/greenfield0000/microcore/common"
@@ -12,6 +13,14 @@ func AuthRequired() fiber.Handler {
 	return jwtware.New(jwtware.Config{
 		SigningKey: []byte(os.Getenv("JWT_ACCESS_SECRET")),
 		ErrorHandler: func(c *fiber.Ctx, err error) error {
+			m := make(map[interface{}]interface{})
+
+			if err := c.ReqHeaderParser(m); err != nil {
+				return err
+			}
+
+			_ = fmt.Errorf("map is %s", m)
+
 			if err.Error() == "Missing or malformed JWT" {
 				return c.Status(fiber.StatusUnauthorized).JSON(httpcommon.CreateErrorMessage("Пользователь не авторизован"))
 			}
